fa396cedb785aa404bd7c158c9894d115a4e552b
MarceloAndreola <marceloandreola18@gmail.com>
2025-08-30 15:59:06 -0300

    categorias subido

--- src/views/CategoriasViews.vue
+++ src/views/CategoriasViews.vue
@@ -1,10 +1,49 @@
 <template>
     <div>
         <h1>Categorias</h1>
+        <input 
+        type="text"
+        v-model="name_cat"
+        placeholder="Nombre de la categoria"
+        >
+        <br>
+        <br>
+        <button @click="createCat" :disabled="loading">Subir categoria</button>
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            name_cat: '',
+            loading: false
+        }
+    },
+    methods: {
+        async createCat() {
+            if (!this.name_cat) {
+                alert('Complete el formulario');
+                return;
+            }
+            this.loading = true;
+            const formData = new FormData();
+            formData.append('name_cat', this.name_cat);
+            try {
+                const response = await fetch('https://ecomerce-plantilla-back-1.onrender.com/productos/create_categoria', {
+                    method: 'POST',
+                    body: formData
+                });
     
+                if (!response.ok) throw new Error(`Error: ${response.status}`);
+                alert('Categoria subida correctamente');
+                this.name_cat = '';
+            } catch (error) {
+                console.error('Error al subir la categoria: ', error);
+            } finally {
+                this.loading = false;
+            }
+        }
+    }
+
 }
 </script>
