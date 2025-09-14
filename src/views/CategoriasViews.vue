a9b28b8b1a29e9ca74156b0f6320537af58f2c84
MarceloAndreola <marceloandreola18@gmail.com>
2025-09-14 14:11:51 -0300

    Actualizacion del front

--- src/views/CategoriasViews.vue
+++ src/views/CategoriasViews.vue
@@ -10,13 +10,50 @@
         <br>
         <button @click="createCat" :disabled="loading">Subir categoria</button>
     </div>
+    <br>
+    <br>
+
+    <!--BUSCADOR DE CATEGORIA-->
+    <div>
+        <h2>Buscar para modificar</h2>
+        <input 
+        type="text"
+        v-model="buscarNombre"
+        placeholder="Buscar por nombre"
+        >
+        <br>
+        <br>
+        <button @click="buscarCategoria">Buscar</button>
+
+        <ul v-if="resultados.length > 0">
+            <li v-for="cat in resultados" :key="cat.id">
+                nombre = {{ cat.name_cat }}
+                <button @click="seleccionarCategoria(cat)">Seleccionar</button>
+            </li>
+        </ul>
+    </div>
+
+    <!--MODIFICAR CATEGORIA-->
+    <div v-if="categoriaId">
+        <input 
+        type="text"
+        v-model="name_categoria"
+        placeholder="Nombre de la categoria"
+        >
+        <br><br>
+        <button @click="modificarCat">Subir modificación</button>
+    </div>
 </template>
 <script>
 export default {
     data() {
         return {
             name_cat: '',
-            loading: false
+            name_categoria: '',
+            resultados: [],
+            loading: false,
+            buscarNombre: '',
+            categoriaId: null,
         }
     },
     methods: {
@@ -45,6 +82,59 @@ export default {
             } finally {
                 this.loading = false;
             }
+        },
+        
+        async buscarCategoria() {
+            if (!this.buscarNombre) {
+                alert('Ingrese un nombre para buscar');
+                return;
+            }
+
+            try {
+                const res = await fetch(`https://ecomerce-plantilla-back-1.onrender.com/productos/buscar_cat/${this.buscarNombre}`);
+                if (!res.ok) throw new Error('Error al buscar categorias');
+                
+                const data = await res.json();
+
+                if (data.length === 0) {
+                    alert('No se encontraron categorías');
+                    this.resultados = [];
+                } else {
+                    this.resultados = data;
+                }
+            } catch (err) {
+                alert(err.message);
+            }
+        },
+
+        seleccionarCategoria(cat) {
+            this.categoriaId = cat.id;
+            this.name_categoria = cat.name_cat
+        }, 
+
+        async modificarCat() {
+            if (!this.categoriaId) {
+                alert('Primero seleccione una categoria');
+                return;
+            }
+
+            const formData = new FormData();
+            formData.append('name_cat', this.name_categoria);
+
+            try {
+                const response = await fetch(`https://ecomerce-plantilla-back-1.onrender.com/productos/modificar_cat/${this.categoriaId}`, {
+                    method: 'PUT',
+                    body: formData
+                });
+
+                if (!response.ok) throw new Error(`Error: ${response
+                .status}`);
+
+                alert('Categoria modificada correctamente')
+            } catch (error) {
+                console.error('Error al modificar la categoria', error);
+            }
+
         }
     }
 
